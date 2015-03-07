5a3341af2916d55637d06b5da9c52d976c79de14
jimwei <jim.wei@grapecity.com>
2015-03-07 09:02:05 +0800

    add files

---
+++ api/sfxCreator.go
@@ -0,0 +1,264 @@
+package api
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const (
+	RAR    = `C:\Program Files\WinRAR\WinRAR.exe`
+	CSEXE  = `SFX\CS_QuickDeployment.exe`
+	ASPEXE = `SFX\ASP_QuickDeployment.exe`
+	ARGS0  = `a -sfx -z%s\sfx.txt`
+	BAT    = "sfx.bat"
+)
+
+//the user setting for quickdepyoment
+type Config struct {
+	//1: client;2:server
+	Location int
+	//1:asp;2:cs;3:asp first,then cs
+	ClientMode int
+}
+type ExeConfig struct {
+	XMLName     xml.Name    `xml:"configuration"`
+	Appsettings Appsettings `xml:"appSettings"`
+}
+
+type Appsettings struct {
+	Comment string `xml:",comment"`
+	Adds    []Add  `xml:"add"`
+}
+
+type Add struct {
+	Key   string `xml:"key,attr"`
+	Value string `xml:"value,attr"`
+}
+
+var (
+	BasePath string
+)
+
+func init() {
+	//set the current work directory
+	setBasePath()
+}
+
+//create the sfx exe
+func CreateSFX(cfg *Config) {
+	//create the sfx exe
+	createSFXCore(cfg)
+}
+
+//set the base path
+func setBasePath() {
+	var file = os.Args[0]
+	fmt.Println("the args[0] is", file)
+	thePath, _ := exec.LookPath(file)
+	BasePath = filepath.Dir(thePath)
+
+}
+
+//modify the config
+func writeConfig(cfg *Config) {
+	cName := filepath.Join(BasePath, `File\QuickDeployment.exe.config`)
+	fmt.Println("the cname is", cName)
+	sb, _ := ioutil.ReadFile(cName)
+	var execfg ExeConfig
+	err := xml.Unmarshal(sb, &execfg)
+	if err != nil {
+		fmt.Println("unmarshal exe config fail.", err.Error())
+		return
+	}
+
+	execfg.Appsettings.Adds[0].Value = strconv.Itoa(cfg.Location)
+	execfg.Appsettings.Adds[1].Value = strconv.Itoa(cfg.ClientMode)
+	fmt.Println("the modifyed app is", execfg)
+	//marshal
+	newb, err := xml.Marshal(&execfg)
+	if err != nil {
+		fmt.Println("marshal fail", err.Error())
+	}
+	ioutil.WriteFile(cName, newb, os.ModePerm)
+}
+
+//reate the sfx exe binary
+func createSFXCore(cfg *Config) {
+	//first delete the sfx directory and the older sfx files
+	os.Remove(filepath.Join(BasePath, CSEXE))
+	os.Remove(filepath.Join(BasePath, ASPEXE))
+	argsList := GetRarArgs(cfg)
+	log.Println("the args list length", len(argsList))
+	for _, arg := range argsList {
+		log.Println("the arg is", arg)
+
+		//write config
+		if strings.Index(arg, ASPEXE) > 0 {
+			//asp model
+			cfg.ClientMode = 1
+			log.Println("write asp config")
+			writeConfig(cfg)
+		} else if strings.Index(arg, CSEXE) > 0 {
+			//cs model
+			cfg.ClientMode = 2
+			log.Println("write cs config")
+			writeConfig(cfg)
+		}
+		args := strings.Split(arg, " ")
+
+		cmd := exec.Command(RAR, args...)
+
+		if err := cmd.Run(); err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println("create sfx successfully!")
+		time.Sleep(1000)
+	}
+	time.Sleep(1000)
+}
+
+//download the sfx files
+func DownloadFile(w http.ResponseWriter) {
+	//delete the older zip file
+	os.Remove(filepath.Join(BasePath, `SFX.zip`))
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Header().Set("Content-Transfer-Encoding", "binary")
+	w.Header().Set("Content-Disposition", "attachment;filename='SFX.zip'")
+
+	err := exec.Command(RAR, "a", "SFX.zip", "-ep1", filepath.Join(BasePath, `SFX\*`)).Run()
+	if err != nil {
+		log.Println("zip files fail.", err.Error())
+		return
+	}
+	f, err := os.Open(filepath.Join(BasePath, `SFX.zip`))
+	if err != nil {
+		log.Println("read the exe file fail.", err.Error())
+		return
+	}
+	defer f.Close()
+	content, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Println("*read the exe file fail.", err.Error())
+		return
+	}
+
+	w.Write(content)
+}
+
+//generate the rar sfx args
+func GetRarArgs(cfg *Config) []string {
+	argsList := []string{}
+
+	argsFileDir := `%s\File`
+	//get the exe name
+	exeNames := GetExeNames(cfg)
+	var args1 = ` %s -ep1 %s`
+	for _, v := range exeNames {
+		arg := fmt.Sprintf(strings.Join([]string{fmt.Sprintf(ARGS0, BasePath), args1}, ""), v, fmt.Sprintf(argsFileDir, BasePath))
+		argsList = append(argsList, arg)
+	}
+
+	return argsList
+}
+
+// get the exe name depend on the client mode
+func GetExeNames(cfg *Config) []string {
+	names := []string{}
+	csName := filepath.Join(BasePath, CSEXE)
+	aspName := filepath.Join(BasePath, ASPEXE)
+	if cfg.ClientMode == 3 {
+		//need produce cs sfx and asp sfx
+		names = append(names, aspName)
+		names = append(names, csName)
+	} else if cfg.ClientMode == 1 {
+		names = append(names, aspName)
+	} else if cfg.ClientMode == 2 {
+		names = append(names, csName)
+	}
+	log.Println("the exenames is", names)
+	return names
+}
+
+//create the dll directory
+//return the directory
+func CreateDirectory(cfg *Config) string {
+	deleteDirectory("Kaikei")
+	deleteDirectory("Server")
+	var thePath string = ""
+	if cfg.Location == 1 {
+		//client
+		thePath = filepath.Join(BasePath, `File\Deployment\Kaikei`)
+		//need delete the server directory
+	} else if cfg.Location == 2 {
+		//server
+		thePath = filepath.Join(BasePath, `File\Deployment`)
+		//need delete the client kaikei directory
+	}
+	_, err := os.Stat(thePath)
+	if err != nil {
+		//no the directory,so create the directory
+		os.Mkdir(thePath, os.ModePerm)
+	}
+
+	return thePath
+}
+
+//delete the upload directory
+func deleteDirectory(directory string) {
+	thePath := filepath.Join(BasePath, `File\Deployment`, directory)
+	log.Println("the delete path is", thePath)
+	err := os.RemoveAll(thePath)
+	if err != nil {
+		log.Println("delete the directory fail.", err.Error())
+	}
+}
+
+//check the uploaded files whether are dll or not
+func CheckFile(files map[string][]*multipart.FileHeader) bool {
+	for _, headers := range files {
+		for _, header := range headers {
+			ext := filepath.Ext(header.Filename)
+			if strings.ToLower(ext) != ".dll" {
+				log.Println("the ext is", ext)
+				log.Println("invalid file", header.Filename)
+				return false
+			}
+		}
+	}
+	return true
+}
+
+//copy the uploaded file content
+func CopyFile(w http.ResponseWriter, header *multipart.FileHeader, cfg *Config, thePath string) bool {
+	//open the file
+	file, err := header.Open()
+	if err != nil {
+		log.Println("open the file fail", err.Error())
+
+		return false
+	}
+	defer file.Close()
+	log.Println("the uploaded filename is", header.Filename)
+
+	//create the file
+	f, _ := os.OpenFile(filepath.Join(thePath, header.Filename), os.O_CREATE|os.O_WRONLY, os.ModePerm)
+	defer f.Close()
+	_, err = io.Copy(f, file)
+	if err != nil {
+		log.Println("upload fail.", err.Error())
+		return false
+	}
+	return true
+}
---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"html/template"
+	"leyserSFX/api"
+	"log"
+	"net/http"
+)
+
+func main() {
+	//router
+	http.HandleFunc("/create", CreateSFXHandler)
+	http.Handle("/vendor/", StaticHandler())
+	http.HandleFunc("/", IndexHandler)
+
+	//listen and serve
+	http.ListenAndServe(":8888", nil)
+}
+func StaticHandler() http.Handler {
+	log.Println("enter static func")
+	return http.FileServer(http.Dir("./static/"))
+
+}
+
+//index html
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("enter index func")
+	t, err := template.ParseFiles("SFX.html")
+	if err != nil {
+		log.Println("load template fail.", err.Error())
+		return
+	}
+	t.Execute(w, nil)
+}
+
+//create sfx files
+func CreateSFXHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(32 << 20)
+
+	cfg := GetConfig(r)
+	log.Println("the cfg is", cfg)
+
+	formdata := r.MultipartForm
+	log.Println("the MultipartForm is", formdata)
+
+	files := formdata.File
+	log.Println("the files is", files)
+	// check files type
+	if api.CheckFile(files) == false {
+		log.Println("check files type fail.")
+		w.Write([]byte("invalid file"))
+		return
+	}
+	//create directory
+	thePath := api.CreateDirectory(cfg)
+	for _, headers := range files {
+		for _, header := range headers {
+			if api.CopyFile(w, header, cfg, thePath) == false {
+				return
+			}
+		}
+
+	}
+	log.Println("the origin config is", cfg)
+	//create sfx
+	api.CreateSFX(cfg)
+
+	//download
+	api.DownloadFile(w)
+}
+
+//get the config
+func GetConfig(r *http.Request) *api.Config {
+	var cfg api.Config
+	cbxASP := r.FormValue("cbxASP")
+	cbxCS := r.FormValue("cbxCS")
+	location := r.FormValue("location")
+	log.Println("the cbxasp value is", cbxASP)
+	log.Println("the cbxcs valus is", cbxCS)
+	log.Println("the location value is", location)
+	//client mode
+	if cbxASP == "on" && cbxCS == "on" {
+		cfg.ClientMode = 3
+	} else if cbxASP == "on" {
+		cfg.ClientMode = 1
+	} else if cbxCS == "on" {
+		cfg.ClientMode = 2
+	}
+	//location
+	if location == "client" {
+		cfg.Location = 1
+	} else if location == "server" {
+		cfg.Location = 2
+	}
+	return &cfg
+}
